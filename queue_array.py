a9dca924db4dc69efe999ede1873019ba3a81e06
ansarshaik <ansarshaik965@gmail.com>
2024-01-08 20:43:38 +0530

    added front queue

--- queue_array.py
+++ queue_array.py
@@ -3,6 +3,8 @@ import array
 class Queue:
     def __init__(self):
         self.my_queue=array.array('i',[])
+    def get_front(self):
+        print(len(self.my_queue)-1)
     def enqueue(self,element):
         self.my_queue.append(element)
     def is_empty(self):
@@ -19,11 +21,18 @@ class Queue:
 if __name__=="__main__":
     queue=Queue()
     print(queue.my_queue)
-
+    queue.get_front()
     queue.enqueue(1)
+    print(queue.my_queue)
+    queue.get_front()
     queue.enqueue(2)
+    print(queue.my_queue)
+    queue.get_front()
     queue.enqueue(3)
     print(queue.my_queue)
     
     queue.dequeue()
     print(queue.my_queue)
+    queue.dequeue()
+    queue.dequeue()
+    queue.dequeue()
